c44a9eb7a57f8ae2591a3dced86c189dc4886492
Jumper Luko <jumper.luko@gmail.com>
2018-10-23 20:11:03 -0300

    Sobre com fotinho

--- sobre.php
+++ sobre.php
@@ -2,5 +2,17 @@
 <?php require('rdd/head.php') ?>
     <div class="headerSpace"></div>
     <div class="vcol-12 centerText h4" style="padding:5%;">Empresa recentemente iniciada em 2018 por Thiago Augusto, com o propósito inicial de oferecer sites com um diferencial de ótima qualidade visual de valor, por um preço acessivel para pequenas empresas. Aproveitando o melhor das tecnologias web disponiveis, trazendo agilidade, baixo custo e qualidade.</div>
+    <div class="fcol-12 evenly">
+        <div>
+            <div class="fcol-12 centerH"><img src="./img/Thiago.jpg" style="border-radius: 100%;filter: grayscale(100%);height:250px;width:250px;"></div>
+            <div class="fcol-12 centerH" style="font-family:Lato;font-size:20px;">Thiago Augusto</div>
+            <div class="fcol-12 centerH" style="font-family:Lato;font-size:15px;color:gray;">Web Design / Web developer</div>
+        </div>
+        <div>
+            <div class="fcol-12 centerH"><img src="./img/Ruan.jpg" style="border-radius: 100%;filter: grayscale(100%);height:250px;width:250px;"></div>
+            <div class="fcol-12 centerH" style="font-family:Lato;font-size:20px;">Ruan Lucas</div>
+            <div class="fcol-12 centerH" style="font-family:Lato;font-size:15px;color:gray;">Contato com o cliente</div>
+        </div>
+    </div>
 	</body>
 </html>

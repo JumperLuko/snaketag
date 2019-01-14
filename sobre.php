cfedfdaba8eef3b6e77aa13f41fda3637c12d1ab
Jumper Luko <jumper.luko@gmail.com>
2019-01-14 20:21:37 -0200

    Little adjusts

--- sobre.php
+++ sobre.php
@@ -3,16 +3,16 @@
     <div class="headerSpace"></div>
     <div class="vcol-12 centerText h4" style="padding:5%;">Empresa recentemente iniciada em 2018 por Thiago Augusto, com o propósito inicial de oferecer sites com um diferencial de ótima qualidade visual de valor, por um preço acessivel para pequenas empresas. Aproveitando o melhor das tecnologias web disponiveis, trazendo agilidade, baixo custo e qualidade.</div>
     <div class="fcol-12 evenly">
-        <div>
+        <a class="ainherit" href="https://t.me/Jumper_Luko" target="_BLANK">
             <div class="fcol-12 centerH"><img src="./img/Thiago.jpg" style="border-radius: 100%;filter: grayscale(100%);height:250px;width:250px;"></div>
             <div class="fcol-12 centerH" style="font-family:Lato;font-size:20px;">Thiago Augusto</div>
             <div class="fcol-12 centerH" style="font-family:Lato;font-size:15px;color:gray;">Web Design / Web developer</div>
-        </div>
-        <div>
+        </a>
+        <a class="ainherit" href="https://t.me/Annaalissia" target="_BLANK">
             <div class="fcol-12 centerH"><img src="./img/Anna.jpg"  style="border-radius: 100%;filter: grayscale(100%);height:250px;width:250px;"></div>
             <div class="fcol-12 centerH" style="font-family:Lato;font-size:20px;">Anna Alice</div>
             <div class="fcol-12 centerH" style="font-family:Lato;font-size:15px;color:gray;">Contato com o cliente</div>
-        </div>
+        </a>
     </div>
 	</body>
 </html>

dfa7b89d3c622ce563566889dd2a841929878fb6
Jumper Luko <jumper.luko@gmail.com>
2020-12-31 21:33:11 -0300

    Mudando para HTML

--- contato.php
+++
@@ -1,9 +0,0 @@
-<html>
-<?php require('rdd/head.php') ?>
-    <div class="headerSpace"></div>
-    <div class="allHeight-min h2 centerText grid" style="width:100%;grid-template-rows: auto 50%;">
-        <div class="flex centerV aroundH wrap"><a href="https://t.me/Jumper_Luko" target="_blank" title="Telegram" style="padding:40px 40px;"><img src="img/telegram_black.svg" style="height: 100px;" alt="Telegram"></a><span>snaketag.contato@gmail.com</span><a href="https://api.whatsapp.com/send?phone=554796534397&text=Ol%C3%A1,%20Estava%20visitando%20o%20site%20snakeag.store.%20Est%C3%A1%20dispon%C3%ADvel?" target="_blank" title="Whatsapp" style="padding:40px 40px;"><img src="img/whatsapp_black.svg" style="height: 100px;" alt="Whatsapp"></a></div>
-        <p class="flex center">Localizado em Joinville/SC</p>
-    </div>
-</body>
-</html>
